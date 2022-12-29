c4b851d43b597fa8b75f53083ed9dbae492cb955
fbgabriana <fbgabriana@gmail.com>
2022-12-29 23:55:14 +0800

    Random updates

--- localserver.js
+++ localserver.js
@@ -45,10 +45,47 @@ const server = http.createServer(async (req, res) => {
 			} else {
 				fs.readDir(req.path).then(files => {
 					res.writeHead(200, {"Content-Type": "text/html"});
-					res.write(`<!DOCTYPE html><html lang="en"><head>`)
-					res.write(`<meta charset="utf-8"><meta http-equiv="Content-Security-Policy" content="default-src 'none'; object-src 'none'; script-src resource: chrome:; connect-src https:; img-src http: data: blob: chrome:; style-src 'unsafe-inline';"><meta name="color-scheme" content="light dark">`);
-					res.write(`<style>h1 {border-bottom: 1px solid #c0c0c0; margin-bottom: 10px; padding-bottom: 10px} .hidden {display: none} a {color: LinkText; text-decoration: none} a:link:hover {text-decoration: underline}  .up { margin-left: 1.5em; padding-left: 24px; background: url("${__dirname}/icons/up.png") left/16px no-repeat } li.dir {list-style: none; margin-left: 0; padding-left: 24px; background: url("${__dirname}/icons/folder.png") left/16px no-repeat; } li.file { list-style: none; margin-left: 0; padding-left: 24px; background: url("${__dirname}/icons/file.svg") left/16px no-repeat } li {line-height: 1.5em;}</style>`);
-					res.write(`</html><body>`)
+					res.write(`<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="color-scheme" content="light dark">
+<link rel="icon" type="image/png" href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABt0lEQVR42oxStZoWQRCs2cXdHTLcHZ6EjAwnQWIkJyQlRt4Cd3d3d1n5d7q7ju1zv/q+mh6taQsk8fn29kPDRo87SDMQcNAUJgIQkBjdAoRKdXjm2mOH0AqS+PlkP8sfp0h93iu/PDji9s2FzSSJVg5ykZqWgfGRr9rAAAQiDFoB1OfyESZEB7iAI0lHwLREQBcQQKqo8p+gNUCguwCNAAUQAcFOb0NNGjT+BbUC2YsHZpWLhC6/m0chqIoM1LKbQIIBwlTQE1xAo9QDGDPYf6rkTpPc92gCUYVJAZjhyZltJ95f3zuvLYRGWWCUNkDL2333McBh4kaLlxg+aTmyL7c2xTjkN4Bt7oE3DBP/3SRz65R/bkmBRPGzcRNHYuzMjaj+fdnaFoJUEdTSXfaHbe7XNnMPyqryPcmfY+zURaAB7SHk9cXSH4fQ5rojgCAVIuqCNWgRhLYLhJB4k3iZfIPtnQiCpjAzeBIRXMA6emAqoEbQSoDdGxFUrxS1AYcpaNbBgyQBGJEOnYOeENKR/iAd1npusI4C75/c3539+nbUjOgZV5CkAU27df40lH+agUdIuA/EAgDmZnwZlhDc0wAAAABJRU5ErkJggg==" />
+<style>
+h1 {border-bottom: 1px solid #c0c0c0; margin-bottom: 10px; padding-bottom: 10px}
+.toolbar {position: relative}
+a {color: LinkText; text-decoration: none}
+a:link:hover {text-decoration: underline} 
+li {list-style: none; line-height: 1.5em;}
+.up {margin-left: 1.5em; padding-left: 24px; background: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAACM0lEQVR42myTA+w1RxRHz+zftmrbdlTbtq04qRGrCmvbDWp9tq3a7tPcub8mj9XZ3eHOGQdJAHw77/LbZuvnWy+c/CIAd+91CMf3bo+bgcBiBAGIZKXb19/zodsAkFT+3px+ssYfyHTQW5tr05dCOf3xN49KaVX9+2zy1dX4XMk+5JflN5MBPL30oVsvnvEyp+18Nt3ZAErQMSFOfelCFvw0HcUloDayljZkX+MmamTAMTe+d+ltZ+1wEaRAX/MAnkJdcujzZyErIiVSzCEvIiq4O83AG7LAkwsfIgAnbncag82jfPPdd9RQyhPkpNJvKJWQBKlYFmQA315n4YPNjwMAZYy0TgAweedLmLzTJSTLIxkWDaVCVfAbbiKjytgmm+EGpMBYW0WwwbZ7lL8anox/UxekaOW544HO0ANAshxuORT/RG5YSrjlwZ3lM955tlQqbtVMlWIhjwzkAVFB8Q9EAAA3AFJ+DR3DO/Pnd3NPi7H117rAzWjpEs8vfIqsGZpaweOfEAAFJKuM0v6kf2iC5pZ9+fmLSZfWBVaKfLLNOXj6lYY0V2lfyVCIsVzmcRV9Y0fx02eTaEwhl2PDrXcjFdYRAohQmS8QEFLCLKGYA0AeEakhCCFDXqxsE0AQACgAQp5w96o0lAXuNASeDKWIvADiHwigfBINpWKtAXJvCEKWgSJNbRvxf4SmrnKDpvZavePu1K/zu/due1X/6Nj90MBd/J2Cic7WjBp/jUdIuA8AUtd65M+PzXIAAAAASUVORK5CYII=") left/16px no-repeat }
+.dir {margin-left: 0; padding-left: 24px; background: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABt0lEQVR42oxStZoWQRCs2cXdHTLcHZ6EjAwnQWIkJyQlRt4Cd3d3d1n5d7q7ju1zv/q+mh6taQsk8fn29kPDRo87SDMQcNAUJgIQkBjdAoRKdXjm2mOH0AqS+PlkP8sfp0h93iu/PDji9s2FzSSJVg5ykZqWgfGRr9rAAAQiDFoB1OfyESZEB7iAI0lHwLREQBcQQKqo8p+gNUCguwCNAAUQAcFOb0NNGjT+BbUC2YsHZpWLhC6/m0chqIoM1LKbQIIBwlTQE1xAo9QDGDPYf6rkTpPc92gCUYVJAZjhyZltJ95f3zuvLYRGWWCUNkDL2333McBh4kaLlxg+aTmyL7c2xTjkN4Bt7oE3DBP/3SRz65R/bkmBRPGzcRNHYuzMjaj+fdnaFoJUEdTSXfaHbe7XNnMPyqryPcmfY+zURaAB7SHk9cXSH4fQ5rojgCAVIuqCNWgRhLYLhJB4k3iZfIPtnQiCpjAzeBIRXMA6emAqoEbQSoDdGxFUrxS1AYcpaNbBgyQBGJEOnYOeENKR/iAd1npusI4C75/c3539+nbUjOgZV5CkAU27df40lH+agUdIuA/EAgDmZnwZlhDc0wAAAABJRU5ErkJggg==") left/16px no-repeat; }
+.file {margin-left: 0; padding-left: 24px; background: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAIAAACQkWg2AAAABnRSTlMAAAAAAABupgeRAAABEElEQVR42nRRx3HDMBC846AHZ7sP54BmWAyrsP588qnwlhqw/k4v5ZwWxM1hzmGRgV1cYqrRarXoH2w2m6qqiqKIR6cPtzc3xMSML2Te7XZZlnW7Pe/91/dX47WRBHuA9oyGmRknzGDjab1ePzw8bLfb6WRalmW4ip9FDVpYSWZgOp12Oh3nXJ7nxoJSGEciteP9y+fH52q1euv38WosqA6T2gGOT44vry7BEQtJkMAMMpa6JagAMcUfWYa4hkkzAc7fFlSjwqCoOUYAF5RjHZPVCFBOtSBGfgUDji3c3jpibeEMQhIMh8NwshqyRsBJgvF4jMs/YlVR5KhgNpuBLzk0OcUiR3CMhcPaOzsZiAAA/AjmaB3WZIkAAAAASUVORK5CYII=") left/16px no-repeat }
+.hidden {display: none}
+p#UI_showHidden {position: absolute; top: -1em; left: 480px;}
+</style>
+<script>
+window.addEventListener("DOMContentLoaded", event => {
+
+	const UI_showHidden = document.querySelector("#UI_showHidden");
+	const check_showHidden = document.querySelector("#UI_showHidden input[type='checkbox']");
+	const list_hiddenItems = document.querySelectorAll(".hidden");
+
+	UI_showHidden.style.display = list_hiddenItems.length ? "block" : "none";
+	check_showHidden.checked = localStorage.getItem("show-hidden") === "true";
+
+	const list_showHidden = event => {
+		for (listItem of document.querySelectorAll(".hidden")) {
+			listItem.style.display = check_showHidden.checked ? "block" : "none";
+		}
+		localStorage.setItem("show-hidden", check_showHidden.checked)
+	}
+	check_showHidden.addEventListener("click", list_showHidden);
+	list_showHidden();
+});
+</script>
+</head>
+<body>
+`)
 					crumbs = req.path.split("/");
 					let crumbPaths = [`<a href="/">${host}</a>`];
 					let path = `/`;
@@ -58,12 +95,14 @@ const server = http.createServer(async (req, res) => {
 							crumbPaths.push(`<a href="${path}">${dir}</a>`);
 						}
 					}
-					res.write(`<h1>Index of ${crumbPaths.join("/")}</h1>`);
+					res.write(`<h1>Index of ${crumbPaths.join("/")}</h1>\n`);
+					res.write(`<div class="toolbar">\n`);
 					if (req.path !== "/") {
-						res.write(`<p id="UI_goUp"><a class="up" href="${app.homepage}${path.split("/").slice(0, -2).join("/")}/">[parent directory]</a></p>`);
+						res.write(`<p id="UI_goUp"><a class="up" href="${app.homepage}${path.split("/").slice(0, -2).join("/")}/">[parent directory]</a></p>\n`);
 					}
-
-					res.write(`<ul>`);
+					res.write(`<p id="UI_showHidden" style="display: none;"><label><input type="checkbox">Show hidden items</label></p>\n`);
+					res.write(`</div>\n`);
+					res.write(`<ul>\n`);
 					let dirs = []; let regular_files = []; href = Object.create(null);
 					for (file of files) {
 						href[file] = req.path + file;
@@ -75,37 +114,38 @@ const server = http.createServer(async (req, res) => {
 					}
 					for (dir of dirs) {
 						if (dir[0] !== ".") {
-							res.write(`<li class="dir"><a href="${href[dir]}/">${dir}</a></li>`);
+							res.write(`<li class="dir"><a href="${href[dir]}/">${dir}</a></li>\n`);
 						}
 					}
 					for (dir of dirs) {
 						if (dir[0] === ".") {
-							res.write(`<li class="dir hidden"><a href="${href[dir]}/">${dir}</a></li>`);
+							res.write(`<li class="dir hidden"><a href="${href[dir]}/">${dir}</a></li>\n`);
 						}
 					}
 					for (regular_file of regular_files) {
 						if (regular_file[0] !== ".") {
 							let iconpathname = "";
-							mimetype = mime.lookup(regular_file)
+							mimetype = mime.lookup(regular_file);
 							if (iconpath && mimetype) {
-								iconpathname = iconpath + "/" + mimetype.replace("/","-")
+								iconpathname = iconpath + "/" + mimetype.replace("/","-");
 								if (fs.existsSync(iconpathname + ".svg")) iconpathname = iconpathname + ".svg";
 								if (fs.existsSync(iconpathname + ".png")) iconpathname = iconpathname + ".png";
 							}
 							if (fs.existsSync(iconpathname)) {
-								res.write(`<li class="file" style="background-image: url(${iconpathname})"><a href="${href[regular_file]}">${regular_file}</a></li>`);
+								res.write(`<li class="file" style="background-image: url(${iconpathname})"><a href="${href[regular_file]}">${regular_file}</a></li>\n`);
 							} else {
-								res.write(`<li class="file"><a href="${href[regular_file]}">${regular_file}</a></li>`);
+								res.write(`<li class="file"><a href="${href[regular_file]}">${regular_file}</a></li>\n`);
 							}
 						}
 					}
 					for (regular_file of regular_files) {
 						if (regular_file[0] === ".") {
-							res.write(`<li class="file hidden"><a href="${href[regular_file]}">${regular_file}</a></li>`);
+							res.write(`<li class="file hidden"><a href="${href[regular_file]}">${regular_file}</a></li>\n`);
 						}
 					}
-					res.write(`</ul>`);
-					res.write(`</body></html>`)
+					res.write(`</ul>
+</body>
+</html>`)
 					res.end();
 				});
 			}
@@ -121,11 +161,10 @@ const server = http.createServer(async (req, res) => {
 			});
 		}
 	} else {
-		if (req.path !== "/favicon.ico") {
-			childProcess.exec(`zenity --info --text='<b>Could not find "${req.path}"</b>.\n\nPlease check the spelling and try again.' --no-wrap`);
-		}
 		res.writeHead(204);
 		res.end();
+		if (req.path == "/favicon.ico") return;
+		childProcess.exec(`zenity --info --text='<b>Could not find "${req.path}"</b>.\n\nPlease check the spelling and try again.' --no-wrap`);
 	}
 }).listen(host.port, host.hostname).on("error", err => {
 	console.log(`\x1b[31m${err.message}\x1b[0m`);
