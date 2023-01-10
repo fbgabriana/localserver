5d0f1f2bc5f81f63c28e5a741fefa6d5acd82ab4
fbgabriana <fbgabriana@gmail.com>
2023-01-10 17:33:30 +0800

    Random updates

--- iconpath.js
+++ iconpath.js
@@ -2,13 +2,14 @@ const freedesktopIcon = require( "freedesktop-icons" );
 const util = require("node:util");
 const exec = util.promisify(require("node:child_process").exec);
 
-const getIconPath = (iconname, context = "mimetypes") => {
+const getIconPath = (iconname, context="mimetypes") => {
 
 	return exec("gsettings get org.gnome.desktop.interface icon-theme").then(proc => {
 		return proc.stdout.trim().replace(/^'|'$/g,"");
 	}).then(async icontheme => {
 		return await freedesktopIcon( [ { name: `${iconname.replace("/","-")}`, context: `${context}`, size: 16 }], [`${icontheme}`] )
-			?? await freedesktopIcon( [ { name: `unknown`, context: "mimetypes", size: 16 }], [`${icontheme}`] );
+			?? await freedesktopIcon( [ { name: `unknown`, context: "mimetypes", size: 16 }], [`${icontheme}`] )
+			?? "";
 	});
 }
 
