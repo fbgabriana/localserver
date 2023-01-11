ecf95375ec4841a753142f9bba4c1891af16ab12
fbgabriana <fbgabriana@gmail.com>
2023-01-11 12:44:19 +0800

    Random updates

--- iconpath.js
+++ iconpath.js
@@ -7,9 +7,12 @@ const getIconPath = (iconname, context="mimetypes") => {
 	return exec("gsettings get org.gnome.desktop.interface icon-theme").then(proc => {
 		return proc.stdout.trim().replace(/^'|'$/g,"");
 	}).then(async icontheme => {
-		return await freedesktopIcon( [ { name: `${(iconname || "unknown").replace("/","-")}`, context: `${context}`, size: 16 }], [`${icontheme}`] )
+		iconname = `${(iconname || "application/octet-stream").replace("/","-")}`;
+		return await freedesktopIcon( [ { name: `${iconname}`, context: `${context}`, size: 16 }], [`${icontheme}`] )
 			?? await freedesktopIcon( [ { name: `unknown`, context: "mimetypes", size: 16 }], [`${icontheme}`] )
 			?? "";
+	}).catch(err => {
+		return; // return undefined on unsupported systems
 	});
 }
 
